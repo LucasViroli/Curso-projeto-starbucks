8b663472ed1fc6ca1074c3411579117bbfb9a1b7
LucasViroli <lucas.viroli@gmail.com>
2026-01-06 13:53:45 -0300

    execuçoes novas com semanticas modernas

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
-let circulo = document.querySelector(".circulo")
-let imagem = document.querySelector(".starbucks")
+const circulo = document.getElementById('circulo')
+const imagem = document.querySelector('.starbucks')
+const botoes = document.querySelectorAll('.botao-menu')
 
-function trocarCor (cor){
+function trocarCor(cor) {
+  if (circulo) {
     circulo.style.background = cor
+  }
 }
 
-function trocaImagem(img){
-    imagem.src = img
+function trocaImagem(src) {
+  if (imagem) {
+    imagem.src = src
+  }
 }
+
+botoes.forEach(botao => {
+  botao.addEventListener('click', () => {
+    const cor = botao.dataset.cor
+    const img = botao.dataset.img
+
+    trocarCor(cor)
+    trocaImagem(img)
+  })
+})
