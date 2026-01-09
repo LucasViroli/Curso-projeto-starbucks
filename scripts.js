4136300f7b61a58c62727b182f2703999e6d5757
LucasViroli <lucas.viroli@gmail.com>
2026-01-09 00:02:48 -0300

    final ate o momento dia 9

--- scripts.js
+++ scripts.js
@@ -3,23 +3,23 @@ const imagem = document.querySelector('.starbucks')
 const botoes = document.querySelectorAll('.botao-menu')
 
 function trocarCor(cor) {
-  if (circulo) {
-    circulo.style.background = cor
-  }
+    if (circulo) {
+        circulo.style.background = cor
+    }
 }
 
 function trocaImagem(src) {
-  if (imagem) {
-    imagem.src = src
-  }
+    if (imagem) {
+        imagem.src = src
+    }
 }
 
 botoes.forEach(botao => {
-  botao.addEventListener('click', () => {
-    const cor = botao.dataset.cor
-    const img = botao.dataset.img
+    botao.addEventListener('click', () => {
+        const cor = botao.dataset.cor
+        const img = botao.dataset.img
 
-    trocarCor(cor)
-    trocaImagem(img)
-  })
+        trocarCor(cor)
+        trocaImagem(img)
+    })
 })
